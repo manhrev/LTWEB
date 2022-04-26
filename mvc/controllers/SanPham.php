896209ce7026fedff6516b54109246e9feb61c20
manhrev <89514697+manhrev@users.noreply.github.com>
2022-04-27 00:45:05 +0700

    add phan trang, edit model

--- mvc/controllers/SanPham.php
+++ mvc/controllers/SanPham.php
@@ -1,17 +1,32 @@
 <?php
 
 class SanPham extends Controller{
-
+    const PER_PAGE = 6;
     
     function BoLoc($filter, $page = 1) {
 
     }
 
     function TatCaSanPham($page = 1) {
+        
         $model= $this->model('SanPhamModel');
-        $a=$model->TatCaSanPham();
-        print_r($a);
-     //$this->view('product-grid', []);
+        $allSP=$model->TatCaSanPham();
+        
+        $sliced_data = array_slice($allSP, ($page-1)*self::PER_PAGE, self::PER_PAGE);
+
+        $number_of_pages = ceil(count($allSP)/self::PER_PAGE);
+
+        if ($page<=0 || $page>$number_of_pages) {
+            header("Location: ".BASE_URL."Error");
+        }
+
+        $this->view('product-grid', [
+            'pageTitle' => 'Tất cả sản phẩm',
+            'numOfSP' => count($allSP),
+            'allSP' => $sliced_data,
+            'currentPage' => $page,
+            'numOfPage' => $number_of_pages,
+        ]);
     }
 
     function GetSanPham($ten) {
--- mvc/models/SanPhamModel.php
+++ mvc/models/SanPhamModel.php
@@ -3,7 +3,7 @@
 class SanPhamModel extends DB {
     public function TatCaSanPham()
     {
-        $query = "select url, name, images, price  from product";
+        $query = "select url, name, images, price, description  from product";
         $result = $this -> con -> query($query);
 
         while ($item = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
