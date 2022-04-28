079dcce88bc0acfa6eb4d9bb9b00c7e66cc01afe
manhrev <89514697+manhrev@users.noreply.github.com>
2022-04-28 22:51:39 +0700

    add them sanpham (worked)

--- mvc/controllers/QuanLi.php
+++ mvc/controllers/QuanLi.php
@@ -37,7 +37,43 @@ class QuanLi extends Controller{
     }
 
     function ThemSanPham() {
-        echo 'them san pham';
+        //if post to this
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $i = 0;
+            $count = count($_POST);
+            //categories
+            $categories = [];
+            foreach ($_POST as $key=>$post) {
+                if ($i>4 && $i<$count-1) {
+                    $categories[] = $post;
+                }
+                $i ++;
+            }
+            print_r($categories);
+            //khac
+            $name = $_POST['name'];
+            $price = $_POST['price'];
+            $view_count = $_POST['view_count'];
+            $images = $_POST['images'];
+            $description = $_POST['description'];
+            $tags = $_POST['tags'];
+    
+
+            $model = $this->model('QuanLiModel');
+            $messsage = $model->ThemSP($name, $price, $images, $description, $tags, $view_count, $categories);
+            echo "<script>
+            alert('".$messsage."');
+            window.location.href='".BASE_URL."quan-li/quan-li-san-pham/';
+            </script>";          
+        } 
+        //not post, render themsp view
+        else {
+            $spModel = $this->model('SanPhamModel');
+            $categories = $spModel->GetCategory(false);
+            $this->view('them-sanpham', [
+                'categories' => $categories
+            ]);
+        }
     }
 
     function ChiTietSanPham($url) {
@@ -69,12 +105,12 @@ class QuanLi extends Controller{
                 //categories
                 $categories = [];
                 foreach ($_POST as $key=>$post) {
-                    echo($key).'<br>';
-                    if ($i>4 && $i<$count-1) {
+                    if ($i>4 && $i<$count-2) {
                         $categories[] = $post;
                     }
                     $i ++;
                 }
+                print_r($categories);
                 //khac
                 $name = $_POST['name'];
                 $price = $_POST['price'];
@@ -85,7 +121,7 @@ class QuanLi extends Controller{
                 $current_url = $_POST['url'];
 
                 $model = $this->model('QuanLiModel');
-                echo $model->SuaSP($current_url, $name, $price, $images, $description, $tags, $view_count, $categories);
+                //echo $model->SuaSP($current_url, $name, $price, $images, $description, $tags, $view_count, $categories);
                
                 
             } else {
---
+++ mvc/views/them-sanpham.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Thêm sản phẩm mới</title>
+</head>
+
+<body>
+    <?php require_once "./mvc/views/inc/ql-header.php" ?>
+
+
+    <main class="container">
+
+        <h3 class="mt-5 pt-5">Thêm sản phẩm mới</h3>
+        <p class="mt-3" style="font-weight: 600"><span style="color: red">Lưu ý: <br></span>+Các đường link của ảnh và các tags được ngăn cách bới dấu phẩy (,). 
+            Ví dụ:<br>Ảnh:<br>&emsp; https://i.imgur.com/3pZHogY.jpeg, https://i.imgur.com/aw9YYgp.jpeg
+            <br>Tags:<br> &emsp; gỗ, tủ, sồi, nhựa
+        </p>
+        <form method="post" action="<?=BASE_URL?>/quan-li/quan-li-san-pham/them-san-pham/">
+            <table class="table border ">
+                <thead>
+                    <tr>
+                        <th scope="col">Thuộc tính</th>
+                        <th scope="col">Giá trị</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">
+                            <label for="name" class="form-label">Tên</label>
+                        </th>
+                        <td>
+                            <input required type="text" class="form-control" id="name" name="name" value="" placeholder="Điền tên sản phẩm">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            <label for="price" class="form-label">Giá</label>
+                        </th>
+                        <td>
+                            <input required type="number" class="form-control" id="price" name="price" value="" placeholder="Điền giá (đơn vị vnđ)">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            <label for="view_count" class="form-label">Lượt xem</label>
+                        </th>
+                        <td>
+                            <input required type="number" class="form-control" id="view_count" name="view_count" value="" placeholder="Giả định số lượt xem bài viết">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            <label for="images" class="form-label">Ảnh</label>
+                        </th>
+                        <td>
+                            <textarea required class="form-control" id="images" name="images" rows="4" placeholder="Điền các đường link ảnh (ngăn cách bởi dấu phẩy ','"></textarea>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            <label for="description" class="form-label">Mô tả</label>
+                        </th>
+                        <td>
+                            <textarea required class="form-control" id="description" name="description" rows="10" placeholder="Viết mô tả về sản phẩm, lưu ý viết như đoạn văn, nên có xuống dòng"></textarea>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Danh mục</th>
+                        <td>
+                            <?php foreach($data['categories'] as $key => $cate) {?>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="category-<?=$key?>" value="<?=$cate['url']?>" id="category-<?=$key?>">
+                                <label class="form-check-label" for="category-<?=$key?>">
+                                    <?=$cate['name']?>
+                                </label>
+                            </div>
+                            <?php }?>
+                            
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            <label for="tags" class="form-label">Tags</label>
+                        </th>
+                        <td>
+                            <input required type="text" class="form-control" id="tags" name="tags" value="" placeholder="Điền các tags liên quan đến sản phẩm (ví dụ: tủ gỗ thì có các tags như: tủ, gỗ, tủ gỗ, tu, go, tu-go,...)">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <button type="submit" class="btn btn-success mb-5">Hoàn thành</button>
+        </form>
+    </main>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
