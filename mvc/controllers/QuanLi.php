a6145d74d956006828ac85b5c2c37a30324ddf65
taidao0109 <19522148@gm.uit.edu.vn>
2022-04-28 23:04:43 +0700

    update hsdhs'

--- mvc/controllers/QuanLi.php
+++ mvc/controllers/QuanLi.php
@@ -22,7 +22,7 @@ class QuanLi extends Controller{
 
         //ThemSP($Name, $Price, $Images, $Description, $Tag, $View_count, $Categorys) (Tên, Giá, link nhiều ảnh, mô tả, view, MẢNG CHỨA URL CÁC DANH MỤC CHỨA SẢN PHẨM)
 
-        // SuaSP($CurrentUrl, $Name, $Price, $Images, $Description, $Tag, $View_count, $Categorys) (URL sản phẩm cần sửa, Tên, Giá, link nhiều ảnh, mô tả, view, MẢNG CHỨA URL CÁC DANH MỤC CHỨA SẢN PHẨM)
+        //SuaSP('giuong-pallet', 'giường pallet xịn', 11220000, 'https://i.imgur.com/j3b9h8y.jpg, https://i.imgur.com/k3VhXAr.jpg', 'sahdjsahdjhasjdhsajhdjash', 'giường, pallet, giường pallet', 0, array('giuong','vach-tv')); //(URL sản phẩm cần sửa, Tên, Giá, link nhiều ảnh, mô tả, view, MẢNG CHỨA URL CÁC DANH MỤC CHỨA SẢN PHẨM)
 
         // XoaSP($CurrentUrl) (URL sản phẩm cần xóa.)
 
