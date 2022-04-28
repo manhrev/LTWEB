5ac006e0e1acf19a471b121907683d77026e8862
manhrev <89514697+manhrev@users.noreply.github.com>
2022-04-28 21:59:29 +0700

    add quan li view

---
+++ mvc/views/inc/ql-header.php
@@ -0,0 +1,26 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Trang chủ</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav w-100">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="ql-sanpham.html">Quản lí sản phẩm</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="ql-danhmuc.html">Quản lí danh mục</a>
+            </li>
+
+          </ul>
+          <div class="d-flex flex-row-reverse w-100">   
+            <a class="btn btn-danger" aria-current="page" href="ql-danhmuc.html">Đăng xuất</a>
+        </div>
+      </div>
+        </div>
+      </div>
+    </nav>
+</header>
