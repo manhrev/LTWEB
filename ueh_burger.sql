1078e8d5b283559be962741f9f3b23014dc45916
taidao0109 <19522148@gm.uit.edu.vn>
2022-05-16 23:21:15 +0700

    sql

--- ueh_burger.sql
+++ ueh_burger.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 16, 2022 lúc 03:30 PM
+-- Thời gian đã tạo: Th5 16, 2022 lúc 06:20 PM
 -- Phiên bản máy phục vụ: 10.4.24-MariaDB
 -- Phiên bản PHP: 7.4.29
 
@@ -60,8 +60,21 @@ CREATE TABLE `category_details` (
 --
 
 INSERT INTO `category_details` (`category_url`, `product_url`) VALUES
+('Burger', 'Burger-Bo-Khoai-Tay'),
 ('Burger', 'Burger-Bo-Nuong-Pho-Mai'),
-('Burger', 'Burger-Bo-Nuong-Whopper');
+('Burger', 'Burger-Bo-Nuong-Whopper'),
+('Burger', 'Burger-Ga-Nuong'),
+('Combo', 'Combo-Bo-tam-pho-mai--banh-lon-'),
+('Combo', 'Combo-Bo-tam-pho-mai--banh-vua-'),
+('Ga-ran', 'Ga-BBQ--1-mieng-'),
+('Ga-ran', 'Ga-gion-khong-cay'),
+('Ga-ran', 'Ga-ran-gion-cay'),
+('Ga-ran', 'Khoai-tay-chien--size-M-'),
+('Mon-an-kem', 'Ga-Nuggets'),
+('Mon-an-kem', 'Mix-wing'),
+('Mon-an-kem', 'Pho-mai-que'),
+('Thuc-uong', 'Milo'),
+('Thuc-uong', 'Nuoc-xoai-dao');
 
 -- --------------------------------------------------------
 
@@ -90,7 +103,31 @@ INSERT INTO `images_store` (`id`, `link`, `date_created`) VALUES
 (10, 'http://localhost/UEH_burger/public/img/sanpham/10.jpg', '2022-05-16 18:04:15'),
 (11, 'http://localhost/UEH_burger/public/img/sanpham/11.jpg', '2022-05-16 18:04:23'),
 (12, 'http://localhost/UEH_burger/public/img/sanpham/12.jpg', '2022-05-16 18:06:04'),
-(13, 'http://localhost/UEH_burger/public/img/sanpham/13.jpg', '2022-05-16 18:06:15');
+(13, 'http://localhost/UEH_burger/public/img/sanpham/13.jpg', '2022-05-16 18:06:15'),
+(14, 'http://localhost/UEH_burger/public/img/sanpham/14.jpg', '2022-05-16 22:35:22'),
+(15, 'http://localhost/UEH_burger/public/img/sanpham/15.jpg', '2022-05-16 22:35:29'),
+(16, 'http://localhost/UEH_burger/public/img/sanpham/16.jpg', '2022-05-16 22:37:26'),
+(17, 'http://localhost/UEH_burger/public/img/sanpham/17.jpg', '2022-05-16 22:38:02'),
+(18, 'http://localhost/UEH_burger/public/img/sanpham/18.jpg', '2022-05-16 22:39:34'),
+(19, 'http://localhost/UEH_burger/public/img/sanpham/19.jpg', '2022-05-16 22:40:37'),
+(20, 'http://localhost/UEH_burger/public/img/sanpham/20.jpg', '2022-05-16 22:40:44'),
+(21, 'http://localhost/UEH_burger/public/img/sanpham/21.jpg', '2022-05-16 22:41:42'),
+(22, 'http://localhost/UEH_burger/public/img/sanpham/22.jpg', '2022-05-16 22:41:58'),
+(23, 'http://localhost/UEH_burger/public/img/sanpham/23.jpg', '2022-05-16 22:43:01'),
+(24, 'http://localhost/UEH_burger/public/img/sanpham/24.jpg', '2022-05-16 22:43:07'),
+(25, 'http://localhost/UEH_burger/public/img/sanpham/25.jpg', '2022-05-16 22:44:01'),
+(26, 'http://localhost/UEH_burger/public/img/sanpham/26.jpg', '2022-05-16 22:44:10'),
+(27, 'http://localhost/UEH_burger/public/img/sanpham/27.jpg', '2022-05-16 22:46:31'),
+(28, 'http://localhost/UEH_burger/public/img/sanpham/28.jpg', '2022-05-16 22:46:39'),
+(29, 'http://localhost/UEH_burger/public/img/sanpham/29.jpg', '2022-05-16 22:49:14'),
+(30, 'http://localhost/UEH_burger/public/img/sanpham/30.jpg', '2022-05-16 22:49:21'),
+(31, 'http://localhost/UEH_burger/public/img/sanpham/31.jpg', '2022-05-16 22:50:21'),
+(32, 'http://localhost/UEH_burger/public/img/sanpham/32.jpg', '2022-05-16 22:50:28'),
+(33, 'http://localhost/UEH_burger/public/img/sanpham/33.jpeg', '2022-05-16 22:52:11'),
+(34, 'http://localhost/UEH_burger/public/img/sanpham/34.jpg', '2022-05-16 22:52:20'),
+(35, 'http://localhost/UEH_burger/public/img/sanpham/35.jpg', '2022-05-16 22:53:11'),
+(36, 'http://localhost/UEH_burger/public/img/sanpham/36.png', '2022-05-16 22:53:17'),
+(37, 'http://localhost/UEH_burger/public/img/sanpham/37.jpg', '2022-05-16 22:55:47');
 
 -- --------------------------------------------------------
 
@@ -129,8 +166,21 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`url`, `name`, `price`, `images`, `description`, `tag`, `date_created`, `view_count`) VALUES
+('Burger-Bo-Khoai-Tay', 'Burger Bò Khoai Tây', '60000', 'http://localhost/UEH_burger/public/img/sanpham/12.jpg, http://localhost/UEH_burger/public/img/sanpham/13.jpg', 'mô tả', 'Burger, Bò Khoai Tây', '2022-05-16', 140),
 ('Burger-Bo-Nuong-Pho-Mai', 'Burger Bò Nướng Phô Mai', '50000', 'http://localhost/UEH_burger/public/img/sanpham/10.jpg, http://localhost/UEH_burger/public/img/sanpham/11.jpg', 'mô tả', 'Burger, Bò Nướng Phô Mai, Phô Mai', '2022-05-16', 299),
-('Burger-Bo-Nuong-Whopper', 'Burger Bò Nướng Whopper', '40000', 'http://localhost/UEH_burger/public/img/sanpham/8.jpg, http://localhost/UEH_burger/public/img/sanpham/8.jpg', 'mô tả', 'burger, Whopper, Bò Nướng Whopper', '2022-05-16', 300);
+('Burger-Bo-Nuong-Whopper', 'Burger Bò Nướng Whopper', '40000', 'http://localhost/UEH_burger/public/img/sanpham/8.jpg, http://localhost/UEH_burger/public/img/sanpham/9.jpg', 'mô tả', 'burger, Whopper, Bò Nướng Whopper', '2022-05-16', 300),
+('Burger-Ga-Nuong', 'Burger Gà Nướng', '45000', 'http://localhost/UEH_burger/public/img/sanpham/14.jpg,http://localhost/UEH_burger/public/img/sanpham/15.jpg', 'mô tả ', 'Burger, Gà Nướng', '2022-05-16', 160),
+('Combo-Bo-tam-pho-mai--banh-lon-', 'Combo Bò tắm phô mai (bánh lớn)', '100000', 'http://localhost/UEH_burger/public/img/sanpham/16.jpg, http://localhost/UEH_burger/public/img/sanpham/17.jpg', 'mô tả', 'Combo, Bò tắm phô mai, Bò tắm phô mai (bánh lớn)', '2022-05-16', 170),
+('Combo-Bo-tam-pho-mai--banh-vua-', 'Combo Bò tắm phô mai (bánh vừa)', '90000', 'http://localhost/UEH_burger/public/img/sanpham/18.jpg, http://localhost/UEH_burger/public/img/sanpham/17.jpg', 'mô tả', 'Combo,  Bò tắm phô mai (bánh vừa)', '2022-05-16', 184),
+('Ga-BBQ--1-mieng-', 'Gà BBQ (1 miếng)', '55000', 'http://localhost/UEH_burger/public/img/sanpham/23.jpg, http://localhost/UEH_burger/public/img/sanpham/24.jpg', 'mô tả', 'Gà BBQ ', '2022-05-16', 140),
+('Ga-gion-khong-cay', 'Gà giòn không cay', '60000', 'http://localhost/UEH_burger/public/img/sanpham/19.jpg, http://localhost/UEH_burger/public/img/sanpham/20.jpg', 'mô tả', 'Gà giòn, không cay', '2022-05-16', 129),
+('Ga-Nuggets', 'Gà Nuggets', '80000', 'http://localhost/UEH_burger/public/img/sanpham/27.jpg, http://localhost/UEH_burger/public/img/sanpham/28.jpg', 'mô tả', 'Gà Nuggets', '2022-05-16', 126),
+('Ga-ran-gion-cay', 'Gà rán giòn cay', '60000', 'http://localhost/UEH_burger/public/img/sanpham/21.jpg, http://localhost/UEH_burger/public/img/sanpham/22.jpg', 'mô tả', 'Gà rán giòn, cay ', '2022-05-16', 130),
+('Khoai-tay-chien--size-M-', 'Khoai tây chiên (size M)', '60000', 'http://localhost/UEH_burger/public/img/sanpham/25.jpg, http://localhost/UEH_burger/public/img/sanpham/26.jpg', 'mô tả', 'Khoai tây chiên ', '2022-05-16', 140),
+('Milo', 'Milo', '40000', 'http://localhost/UEH_burger/public/img/sanpham/35.jpg, http://localhost/UEH_burger/public/img/sanpham/37.jpg', 'milo', 'milo', '2022-05-16', 24),
+('Mix-wing', 'Mix wing', '43000', 'http://localhost/UEH_burger/public/img/sanpham/31.jpg, http://localhost/UEH_burger/public/img/sanpham/32.jpg', 'mô tả', 'Mix wing', '2022-05-16', 23),
+('Nuoc-xoai-dao', 'Nước xoài đào', '45000', 'http://localhost/UEH_burger/public/img/sanpham/33.jpeg, http://localhost/UEH_burger/public/img/sanpham/34.jpeg', 'mô tả', 'Nước xoài đào', '2022-05-16', 57),
+('Pho-mai-que', 'Phô mai que', '40000', 'http://localhost/UEH_burger/public/img/sanpham/29.jpg, http://localhost/UEH_burger/public/img/sanpham/30.jpg', 'mô tả', 'Phô mai que', '2022-05-16', 216);
 
 -- --------------------------------------------------------
 
@@ -166,7 +216,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`username`, `password`, `name`, `address`, `phone_number`, `date_created`, `role`) VALUES
-('taidao', '123', 'Tài Đào', 'chung cư an bình ', '0836456628', '2022-05-16 19:24:22', 0);
+('taidao', '123', 'Tài Đào', 'chung cư an bình ', '0836456628', '2022-05-16 19:24:22', 1);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -223,7 +273,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT cho bảng `images_store`
 --
 ALTER TABLE `images_store`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT cho bảng `order_infor`
