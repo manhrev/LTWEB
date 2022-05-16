a4d7d3cd24a013179cceb0b039f2ee7a26ad2024
manhrev <89514697+manhrev@users.noreply.github.com>
2022-05-16 17:33:52 +0700

    edit base url

--- mvc/config/config.php
+++ mvc/config/config.php
@@ -1,6 +1,6 @@
 <?php
-define('BASE_URL', 'http://localhost/NoithatPage/');
-define('STATIC_URL', 'http://localhost/NoithatPage/public/');
+define('BASE_URL', 'http://localhost/UEH_burger/');
+define('STATIC_URL', 'http://localhost/UEH_burger/public/');
 // define('BASE_URL', 'http://192.168.2.9/NoithatPage/');
 // define('STATIC_URL', 'http://192.168.2.9/NoithatPage/public/');
 ?>
